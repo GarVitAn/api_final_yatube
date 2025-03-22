838ac9b4f191a0c0a20da5325ac0f863b9bf8862
GarVitAn <82102756+GarVitAn@users.noreply.github.com>
2025-03-22 11:18:20 +0700

    Update permissions.py

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -1,16 +1,13 @@
 from rest_framework import permissions
 
 
-class OwnershipPermission(permissions.BasePermission):
+class ReadOnlyOrAuthorPermission(permissions.BasePermission):
+    """
+    Разрешает чтение всем, но изменять или удалять объект может только его автор.
+    """
 
     def has_permission(self, request, view):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
-        )
+        return request.method in permissions.SAFE_METHODS
 
     def has_object_permission(self, request, view, obj):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or obj.author == request.user
-        )
+        return request.method in permissions.SAFE_METHODS or obj.author == request.user
