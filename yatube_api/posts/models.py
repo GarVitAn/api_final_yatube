59cf16b6f2358f7556d7be5dbd1abf55d7905dda
GarVitAn <82102756+GarVitAn@users.noreply.github.com>
2025-03-22 11:07:21 +0700

    Post sort addeted

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -8,7 +8,7 @@ class Comment(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='comments')
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments')
+        'Post', on_delete=models.CASCADE, related_name='comments')
     text = models.TextField()
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
@@ -23,7 +23,10 @@ class Follow(models.Model):
         User,
         on_delete=models.CASCADE,
         related_name='following')
-    
+
+    class Meta:
+        unique_together = ('user', 'following')
+
 
 class Group(models.Model):
     title = models.CharField(max_length=200)
@@ -42,8 +45,11 @@ class Post(models.Model):
     image = models.ImageField(
         upload_to='posts/', null=True, blank=True)
     group = models.ForeignKey(
-        Group, on_delete=models.CASCADE, related_name='posts',
+        Group, on_delete=models.SET_NULL, related_name='posts',
         null=True, blank=True)
 
+    class Meta:
+        ordering = ['-pub_date']  # Сортировка по убыванию даты публикации
+
     def __str__(self):
-        return self.text
+        return self.text[:50]
